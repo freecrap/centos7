b0a87662d6d0baff6a47279de82c514b535db1ad
CheckThisOut <86164655+freecrap@users.noreply.github.com>
2021-06-23 01:58:22 +0100

    Update 40ruby-rails.sh

--- src/scripts/40ruby-rails.sh
+++ src/scripts/40ruby-rails.sh
@@ -9,6 +9,8 @@ echo "==========================================================================
 # Some problem here with the gpg keys...
 gpg2 --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3
 gpg2 --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+echo 409B6B1796C275462A1703113804BB82D39DC0E3:6: | gpg2 --import-ownertrust # mpapis@gmail.com
+echo 7D2BAF1CF37B13E2069D6956105BD0E739499BDB:6: | gpg2 --import-ownertrust 
 curl -sSL https://get.rvm.io | bash -s stable
 
 source /etc/profile.d/rvm.sh
