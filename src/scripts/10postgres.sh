5ff69a1414b0bb139040230335b03f5a2b91fef8
CheckThisOut <86164655+nicksterious@users.noreply.github.com>
2021-08-31 19:19:56 +0100

    Update 10postgres.sh

--- src/scripts/10postgres.sh
+++ src/scripts/10postgres.sh
@@ -12,7 +12,10 @@ cd /root
 yum install centos-release-scl -y
 
 # must install the repo first
-wget https://yum.postgresql.org/11/redhat/rhel-7-x86_64/pgdg-redhat-repo-latest.noarch.rpm
+#wget https://yum.postgresql.org/11/redhat/rhel-7-x86_64/pgdg-redhat-repo-latest.noarch.rpm # no longer works
+# updated
+sudo yum install -y https://download.postgresql.org/pub/repos/yum/reporpms/EL-7-x86_64/pgdg-redhat-repo-latest.noarch.rpm
+
 yum -y install pgdg-redhat-repo-latest.noarch.rpm
 yum -y --nogpgcheck install postgresql11* libpq libpq-devel libpqxx libpqxx-devel
 rm -rf pgdg-redhat-repo-latest.noarch.rpm
